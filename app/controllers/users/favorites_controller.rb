fe5a34fb8a1d02e8f171efde714188dca20fcfec
vagrant <vagrant@localhost.localdomain>
2020-06-06 09:15:11 +0000

    [add]いいね機能

--- app/controllers/users/favorites_controller.rb
+++ app/controllers/users/favorites_controller.rb
@@ -1,2 +1,21 @@
 class Users::FavoritesController < ApplicationController
+
+	before_action :authenticate_user!
+
+	def create
+        @post = Post.find(params[:post_id])
+        @favorite = Favorite.new(post_id: @post.id)
+        @favorite.user_id = current_user.id
+        @favorite.save
+        redirect_back(fallback_location: root_path)
+        end
+
+    def destroy
+        @post = Post.find(params[:post_id])
+        @favorite = Favorite.find_by(post_id: @post.id)
+        @favorite.user_id = current_user.id
+        @favorite.destroy
+        redirect_back(fallback_location: root_path)
+    end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,14 @@ class Post < ApplicationRecord
 	belongs_to :user
 	belongs_to :genre
 
+	has_many :favorites, dependent: :destroy
+
 	enum status:{販売中:0, 販売停止:1, 売切:2}
 
 	attachment :image
+
+	def favorited_by?(user) #投稿にログイン中ユーザーがいいねしているかどうか
+        favorites.where(user_id: user.id).exists?
+    end
 
 end
