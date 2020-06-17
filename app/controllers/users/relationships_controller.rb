ebe8d28c83627510bd0d20881b459ecf8f5d5ca1
vagrant <vagrant@localhost.localdomain>
2020-06-17 21:00:06 +0000

    [fix]Aboutページ

--- app/controllers/users/relationships_controller.rb
+++ app/controllers/users/relationships_controller.rb
@@ -3,12 +3,14 @@ class Users::RelationshipsController < ApplicationController
   before_action :authenticate_user!
 
 	def create
-    	current_user.follow(params[:user_id])
+        @user = current_user
+        @user.follow(params[:user_id])
     	redirect_to request.referer
   	end
 
   	def destroy
-    	current_user.unfollow(params[:user_id])
+        @user = current_user
+    	@user.unfollow(params[:user_id])
     	redirect_to request.referer
   	end
 
